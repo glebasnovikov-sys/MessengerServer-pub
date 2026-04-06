6e2c300fd57db18d436dca2ee8e4a439d7af05ec
glebasnovikov-sys <@email.com>
2026-04-06 07:40:45 +0300

    migration remove fcm token

---
+++ Migrations/20260406044040_RemoveFcmToken.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessengerServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFcmToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FcmToken",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FcmToken",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
